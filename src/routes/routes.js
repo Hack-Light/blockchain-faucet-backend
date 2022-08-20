70d8c80938f52dfd768d1c2c46340d1a37876db5
Light <onohsomtochukwubasil@gmail.com>
2022-08-20 17:37:29 +0100

    fixed linting issue

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable strict */
 /* 
     This is a route function
     containing route to main components
@@ -5,33 +6,32 @@
 */
 const express = require("express")
 const router = express.Router()
-const dataParser = require('body-parser')
-express().use(dataParser.json({extended:true}))
-//creating main routing functions
-//using POST for main functions
-//new transaction request endpoint
-router.post("/new", (req, res) =>{
+const dataParser = require("body-parser")
+express().use(dataParser.json({ extended: true }))
+// creating main routing functions
+// using POST for main functions
+// new transaction request endpoint
+router.post("/new", (req, res) => {
     res.send("Hi, this a new transaction request API endpoint")
 })
 
-//Get transaction request status endpoint
-router.post("/tx", (req, res) =>{
+// Get transaction request status endpoint
+router.post("/tx", (req, res) => {
     res.send("Hi, this a get transaction request status API endpoint")
 })
 
-//listen to 404 request
-router.get("*", (req, res) =>{
+// listen to 404 request
+router.get("*", (req, res) => {
     res.status(404).json({
         success: false,
         message: "Page not found",
         error: {
             statusCode: 404,
             message:
-                "You are trying to access a route that is not defined on this server."
-        }
+                "You are trying to access a route that is not defined on this server.",
+        },
     })
 })
 
-//exports router
+// exports router
 module.exports = router
-    
